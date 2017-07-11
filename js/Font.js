bf81d0d0952cc66120ed8b5db82e4f61429ccdb3
burodepeper <david@burodepeper.nl>
2017-07-11 15:51:15 +0200

    Add some basic styling

--- js/Font.js
+++ js/Font.js
@@ -3,19 +3,31 @@ class Font {
     this.name = name
     this.container = container
     this.element = document.createElement('div')
+    this.element.classList.add('font')
     this.element.style.fontFamily = name
     this.container.appendChild(this.element)
-    this.createTypo()
+    this.createTitle()
+    this.createSamples()
   }
 
-  createTypo() {
+  createTitle() {
+    const title = document.createElement('div')
+    title.classList.add('title')
+    title.textContent = this.name
+    this.element.appendChild(title)
+  }
+
+  createSamples() {
     const sizes = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     this.samples = []
+    this.sampleContainer = document.createElement('div')
+    this.sampleContainer.classList.add('samples')
+    this.element.appendChild(this.sampleContainer)
     for (const size of sizes) {
       const sample = document.createElement('div')
       sample.style.fontSize = size + 'px'
       this.samples.push(sample)
-      this.element.appendChild(sample)
+      this.sampleContainer.appendChild(sample)
     }
   }
 
